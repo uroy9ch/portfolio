50595ab7f004bd4d5357a4e53175c4b9d65f62df
Chris Roy <cmr223@miami.edu>
2015-11-05 09:41:58 -0500

    my message

--- Gruntfile.js
+++ Gruntfile.js
@@ -183,7 +183,7 @@ module.exports = function (grunt) {
                 options: {
                     watchTask: true,
                     // fill in proxy address of local WP server
-                    proxy: ""
+                    proxy: "thechristopher.miami:8888"
                 }
             }
         }
